0868069e95593658d684a7d16fb1fdeb93b3e2af
Nosieek <mateusz.nosko13@gmail.com>
2023-05-06 20:37:14 +0200

    Defined MovieRepository interface

---
+++ backend/movies/src/main/java/com/javact/movies/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.javact.movies.repositories;
+
+import com.javact.movies.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
