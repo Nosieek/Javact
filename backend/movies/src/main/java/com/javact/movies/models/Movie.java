e787ffa8cdfb4d0cc0023961d16ca9b7a073af16
Nosieek <mateusz.nosko13@gmail.com>
2023-05-06 20:33:41 +0200

    Created movie model class

---
+++ backend/movies/src/main/java/com/javact/movies/models/Movie.java
@@ -0,0 +1,24 @@
+package com.javact.movies.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String overview;
+    private String relaseDate;
+    private String posterPath;
+
+}
