2c2370cc6433d17859af8d2865ae52aad862ddd5
Nosieek <mateusz.nosko13@gmail.com>
2023-05-06 21:45:06 +0200

    Implement MovieController with endpoints for movies

---
+++ backend/movies/src/main/java/com/javact/movies/controllers/MovieController.java
@@ -0,0 +1,42 @@
+package com.javact.movies.controllers;
+
+import com.javact.movies.models.Movie;
+import com.javact.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/popular")
+    public List<Movie> getMovies() {
+        return movieService.getPopularMovies();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Movie movie = movieService.getFilmById(id);
+        if (movie != null) {
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping
+    public void saveFilm(@RequestBody Movie film) {
+        movieService.saveFilm(film);
+    }
+}
