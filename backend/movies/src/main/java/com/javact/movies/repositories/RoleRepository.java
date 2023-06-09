2e31edaf2df75a9d03a0272c8a3ffb62d1587393
Nosieek <mateusz.nosko13@gmail.com>
2023-06-09 10:07:37 +0200

    Add RoleRepository and UserRepository interfaces

---
+++ backend/movies/src/main/java/com/javact/movies/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.javact.movies.repositories;
+
+import com.javact.movies.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
