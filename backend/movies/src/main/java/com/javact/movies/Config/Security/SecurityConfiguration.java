a57ee8b4edf7b48e66f68b2550d39f6f2545006b
Nosieek <mateusz.nosko13@gmail.com>
2023-06-12 12:22:49 +0200

    Configure authentication and password encoding

---
+++ backend/movies/src/main/java/com/javact/movies/Config/Security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.javact.movies.Config.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable()
+//                .authorizeHttpRequests((authorize) ->
+//                        authorize.requestMatchers(HttpMethod.GET, "/api/movies/popular").permitAll() //Zezwalamy każdemu na dostęp do interfejsu API REST logowania
+//                                .requestMatchers("/api/auth/**").permitAll()
+////                                .requestMatchers("/api/movies/watchlist").permitAll()
+////                                .anyRequest().permitAll()
+//
+//                );
+//
+//        return http.build();
+//    }
+}
