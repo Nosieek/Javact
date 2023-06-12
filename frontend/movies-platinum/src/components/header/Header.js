a70c07e296fd74b7f56df05bb94acac65bd729f1
Nosieek <mateusz.nosko13@gmail.com>
2023-06-12 21:14:46 +0200

    Refactored Header component to include login/logout functionality

--- frontend/movies-platinum/src/components/header/Header.js
+++ frontend/movies-platinum/src/components/header/Header.js
@@ -1,39 +1,59 @@
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilm, faPager, faPeopleCarryBox, faPingPongPaddleBall, faStairs, faStar, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+import { useCookies } from 'react-cookie';
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {NavLink} from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
- 
-return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-        <Container fluid>
-            <Navbar.Brand href="/" style={{"color":'orange'}}>
-                <FontAwesomeIcon icon ={faFilm}/> MovieSpot
-                <FontAwesomeIcon icon="fa-thin fa-popcorn" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{maxHeight: '100px'}}
-                        navbarScroll
-                    >
-                    <NavLink className ="nav-link" to="/">Home</NavLink>
-                    <NavLink className ="nav-link" to="/popular">Watch List</NavLink>
-                    <NavLink className ="nav-link" to="/popular">Genres</NavLink>
-                    <NavLink className ="nav-link" to="/popular">Ranking</NavLink>    
-                    <NavLink className ="nav-link" to="/popular">About</NavLink>       
-                </Nav>
-                <Button variant="outline-info" className="me-2" style={{"color":'white'}}>Login</Button>
-                <Button variant="outline-info" style={{"color":'orange'}}>Register</Button>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
-  )
-}
+  const [cookies, setCookie, removeCookie] = useCookies(['username']);
+  const navigate = useNavigate();
 
-export default Header
+  const handleLogin = () => {
+    setCookie('username', 'uzytkownik', { path: '/' });
+    navigate('/profile');
+  };
+
+  const handleLogout = () => {
+    removeCookie('username', { path: '/' });
+    navigate('/');
+  };
+
+  const isLoggedIn = !cookies.username;
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" style={{ color: 'orange' }}>
+          <FontAwesomeIcon icon={faFilm} /> MovieSpot
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <NavLink className="nav-link" to="/">Home</NavLink>
+            <NavLink className="nav-link" to="/MovieList">Watch List</NavLink>
+            <NavLink className="nav-link" to="/popular">Genres</NavLink>
+            <NavLink className="nav-link" to="/popular">Ranking</NavLink>
+            <NavLink className="nav-link" to="/popular">About</NavLink>
+          </Nav>
+          {isLoggedIn ? (
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+              <Nav.Link as={Link} to="/profile">{cookies.username}</Nav.Link>
+              <Button variant="outline-info" style={{ color: 'orange' }} onClick={handleLogout}>Logout</Button>
+            </Nav>
+          ) : (
+            <>
+              <Button variant="outline-info" style={{ color: 'orange' }} as={Link} to="/Login" onClick={handleLogin}>Login</Button>
+              <Button variant="outline-info" style={{ color: 'orange' }} as={Link} to="/Register">Register</Button>
+            </>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
