72fd2005887176d2425ed18695402b5cfdb312e4
Nosieek <mateusz.nosko13@gmail.com>
2023-06-14 20:49:15 +0200

    Update Register Component with Form and Styling

--- frontend/movies-platinum/src/components/Login-registration/Register.js
+++ frontend/movies-platinum/src/components/Login-registration/Register.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import './Register.css';
 
 const Register = () => {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const navigate = useNavigate(); // Get the navigate function from React Router
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies(['token']);
+  const [passwordMismatchError, setPasswordMismatchError] = useState(false);
+  const [duplicateUserError, setDuplicateUserError] = useState(false);
 
   const handleRegister = async (e) => {
     e.preventDefault();
+
+    // Sprawdź, czy hasła się zgadzają
+    if (password !== repeatPassword) {
+      setPasswordMismatchError(true);
+      return;
+    }
+
     const registerData = {
       name,
       username,
@@ -19,55 +32,82 @@ const Register = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:8080/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(registerData),
-      });
+      const response = await axios.post('http://localhost:8080/api/auth/register', registerData);
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Registration successful!', data);
-        navigate('/Login'); // Redirect to the login page
-
-      } else {
-        console.log('Registration failed!');
-        // Handle registration failure, e.g., display error message
+      if (response.status === 200) {
+        const { token } = response.data;
+        console.log('Registration successful!', token);
+        setCookie('token', token, { path: '/' });
+        navigate('/');
       }
     } catch (error) {
-      console.error('Error occurred during registration:', error);
-      // Handle error, e.g., display error message
+      if (error.response && error.response.status === 400) {
+        setDuplicateUserError(true);
+      } else {
+        console.error('Error occurred during registration:', error);
+      }
     }
   };
 
+  const handleResetErrors = () => {
+    setPasswordMismatchError(false);
+    setDuplicateUserError(false);
+  };
+
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleRegister}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Register</button>
-      </form>
+    <div className="register-container">
+      <div className="register-form">
+        <form onSubmit={handleRegister}>
+          <h2>Register</h2>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <label htmlFor="repeat-password">Repeat Password:</label>
+          <input
+            type="password"
+            id="repeat-password"
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            required
+          />
+
+          {passwordMismatchError && <p className="error">Passwords do not match.</p>}
+          {duplicateUserError && <p className="error">Username or email already exists.</p>}
+
+          <button className="register-submit-button" type="submit" onClick={handleResetErrors}>
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
