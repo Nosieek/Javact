c77c0b2c517d2c940fbdcab548636b8e2f4d6d2f
Nosieek <mateusz.nosko13@gmail.com>
2023-06-21 11:05:14 +0200

    Refactor movie model

--- backend/movies/src/main/java/com/javact/movies/models/Movie.java
+++ backend/movies/src/main/java/com/javact/movies/models/Movie.java
@@ -21,24 +21,8 @@ public class Movie {
     @Column
     private Long imdb_id;
     private String title;
-
-    public Long getImdb_id() {
-        return imdb_id;
-    }
-
     @Column(length = 1000)
     private String overview;
-
-    public Movie(Long id, Long imdb_id, String title, String overview, String relaseDate, String posterPath, String ytTrailer) {
-        this.id = id;
-        this.imdb_id = imdb_id;
-        this.title = title;
-        this.overview = overview;
-        this.relaseDate = relaseDate;
-        this.posterPath = posterPath;
-        this.ytTrailer = ytTrailer;
-    }
-
     private String relaseDate;
     private String posterPath;
     private String ytTrailer;
