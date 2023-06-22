759701d79a4181f616c476b2c3e5987ad3b51ccb
Nosieek <mateusz.nosko13@gmail.com>
2023-06-22 18:36:48 +0200

    Update cors

--- backend/movies/src/main/java/com/javact/movies/Config/Security/SecurityConfiguration.java
+++ backend/movies/src/main/java/com/javact/movies/Config/Security/SecurityConfiguration.java
@@ -5,11 +5,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -22,17 +31,24 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf().disable().authorizeHttpRequests()
-//                .requestMatchers("/api/auth/**")
-//                .permitAll()
-//                .requestMatchers("/api/movies/popular").permitAll() //Zezwalamy każdemu na dostęp do interfejsu API REST logowania
-//                .anyRequest().authenticated().and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
-        http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
-
+        http.cors(Customizer.withDefaults()).csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .requestMatchers("/api/movies/popular").permitAll()//Zezwalamy każdemu na dostęp do interfejsu API REST logowania
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
+//        http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
         return http.build();
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+//        configuration.setAllowedHeaders(List.of());
+//    }
 }
