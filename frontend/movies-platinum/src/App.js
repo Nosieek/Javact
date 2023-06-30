8b03541da30161af46b8c5ea4d81563bfe3d62f7
Nosieek <mateusz.nosko13@gmail.com>
2023-06-30 16:12:05 +0200

    Added new Route

--- frontend/movies-platinum/src/App.js
+++ frontend/movies-platinum/src/App.js
@@ -8,13 +8,14 @@ import NotFound from './components/notFound/NotFound';
 import Login from './components/Login-registration/Login';
 import TopRanked from './components/Top/TopRanked';
 import Favorites from './components/favorites/favorites';
-
+import MovieDetail from './components/movie-detail/movie-detail';
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/movie/:movieId" element={<MovieDetail />} />
         <Route path="/Trailer/:ytTrailer" element={<Trailer />} />
         <Route path="/Top" element={<TopRanked />} />
         <Route path="/Fav" element={<Favorites />} />
