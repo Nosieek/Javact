ca9e25f4d9d0c06cd2ce529b7b566d2f5850ec43
Nosieek <mateusz.nosko13@gmail.com>
2023-08-16 19:41:31 +0200

    Added button Favorites to Carousel

--- frontend/movies-platinum/src/components/hero/Hero.js
+++ frontend/movies-platinum/src/components/hero/Hero.js
@@ -4,8 +4,39 @@ import { Paper } from "@mui/material";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import {Link, useNavigate} from "react-router-dom";
+import { faHeartCircleMinus, faHeartCirclePlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useCookies } from 'react-cookie';
+import jwtDecode from 'jwt-decode';
+import axios from "axios";
 
 const Hero = ({movies}) => {
+    const [cookies] = useCookies(['token']);
+
+    const getUserEmailFromToken = (token) => {
+        const decodedToken = jwtDecode(token);
+        const userEmail = decodedToken.sub;
+        return userEmail;
+      };
+  
+    const addToFavorite = async (movieId) => {
+      try {
+        const userEmail = getUserEmailFromToken(cookies.token);
+        const tk = cookies.token;
+        console.log(movieId)
+        const response = await axios.post(
+          `http://localhost:8080/api/movies/addFav?email=${userEmail}&movieId=${movieId}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${tk}`
+            }
+          }
+        );
+      } catch (error) {
+        console.error('Error adding movie to favoritelist:', error);
+      }
+    };
+  
     return (
         <div className ='movie-carousel-container'>
           <Carousel>
@@ -25,12 +56,34 @@ const Hero = ({movies}) => {
                                         </div>
 
                                         <div className="movie-buttons-container">
+                                        {cookies.token ? ( 
+                                            <>
                                             <Link to={`/Trailer/${movie.ytTrailer}`}>
                                                 <div className="play-button-icon-container">
                                                     <FontAwesomeIcon className="play-button-icon"
                                                         icon = {faCirclePlay}/>
                                                 </div>
-                                            </Link>
+
+                                             </Link>
+                                             <div className="play-button-icon-container2">
+                                             <FontAwesomeIcon
+                                                        className="heart-icon-hero"
+                                                        icon={faHeartCirclePlus}
+                                                        onClick={() => addToFavorite(movie.id)}
+                                                    />
+                                            </div>
+                                            </>
+                                        ):(
+                                            <Link to={`/Trailer/${movie.ytTrailer}`}>
+                                                <div className="play-button-icon-container">
+                                                    <FontAwesomeIcon className="play-button-icon"
+                                                        icon = {faCirclePlay}/>
+                                                </div>
+
+                                             </Link>
+                                        )}
+
+                                                </div>
                                             {/* <div className="movie-review-button-container">
                                             <Button variant ="info" onClick={() => reviews(??)} >Reviews</Button>
                                             </div> */}
@@ -38,7 +91,6 @@ const Hero = ({movies}) => {
                                         
                                     </div>
                                 </div>
-                            </div>
                         </Paper>
                     )
                 })
