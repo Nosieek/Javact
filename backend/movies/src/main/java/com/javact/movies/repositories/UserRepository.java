76d04f6c5356bb9aa796d63ab5ca4c1cd998ab3b
Nosieek <mateusz.nosko13@gmail.com>
2023-08-23 22:27:03 +0200

    Added query findbyUsername

--- backend/movies/src/main/java/com/javact/movies/repositories/UserRepository.java
+++ backend/movies/src/main/java/com/javact/movies/repositories/UserRepository.java
@@ -11,7 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> { //zmiana na 
     @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 //    Optional<User> findByUsernameOrEmail(String username, String email);
-//    Optional<User> findByUsername(String username);
+    @Query("SELECT n FROM User n WHERE n.username = :username")
+    Optional<User> findByUsername(String username);
 //    Boolean existsByUsername(String username);
 //    Boolean existsByEmail(String email);
 }
