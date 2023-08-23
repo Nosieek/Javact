f8cdb2bf0990d2ac368d005079b0b1bf7b356386
Nosieek <mateusz.nosko13@gmail.com>
2023-08-23 22:20:54 +0200

    Clean up code

--- backend/movies/src/main/java/com/javact/movies/Config/Security/SecurityConfiguration.java
+++ backend/movies/src/main/java/com/javact/movies/Config/Security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.javact.movies.Config.Security;
 
-import io.netty.handler.codec.http.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,46 +13,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
 
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfiguration {
-//
-//    private final AuthenticationProvider authenticationProvider;
-//    private final SecurityJwtConfig jwtAuthFiler;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.cors(Customizer.withDefaults()).csrf().disable().authorizeHttpRequests()
-//                .requestMatchers("/api/auth/**")
-//                .permitAll()
-//                .requestMatchers("/api/movies/popular").permitAll()
-//                .requestMatchers("/api/movies/Fav/delete/**").authenticated()//Zezwalamy każdemu na dostęp do interfejsu API REST logowania
-//                .anyRequest().authenticated().and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthFiler, UsernamePasswordAuthenticationFilter.class);
-////        http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
-//        return http.build();
-//    }
-//
-////    @Bean
-////    CorsConfigurationSource corsConfigurationSource() {
-////        CorsConfiguration configuration = new CorsConfiguration();
-////        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
-////        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-////        configuration.setAllowedHeaders(List.of());
-////    }
-//}
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
---
+++ backend/movies/src/main/java/com/javact/movies/models/Review.java
@@ -0,0 +1,2 @@
+package com.javact.movies.models;public class Review {
+}
