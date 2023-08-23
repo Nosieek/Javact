6704efadec55ddedb0afeccfcecf1527d23a3cf8
Nosieek <mateusz.nosko13@gmail.com>
2023-08-23 22:32:05 +0200

    Created review model

--- backend/movies/src/main/java/com/javact/movies/models/Review.java
+++ backend/movies/src/main/java/com/javact/movies/models/Review.java
@@ -1,2 +1,30 @@
-package com.javact.movies.models;public class Review {
-}
+//package com.javact.movies.models;
+//
+//import com.javact.movies.entity.User;
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table
+//public class Review {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "movie_id")
+//    private Movie movie;
+//
+//    private int rating;
+//
+//    @Column(length = 1000)
+//    private String comment;
+//}
