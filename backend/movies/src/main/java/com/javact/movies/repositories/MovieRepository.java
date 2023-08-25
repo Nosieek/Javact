9ee30641382eadb3ce98452dbed6fa123b3fbf81
Nosieek <mateusz.nosko13@gmail.com>
2023-08-25 12:22:17 +0200

    update

--- backend/movies/src/main/java/com/javact/movies/repositories/MovieRepository.java
+++ backend/movies/src/main/java/com/javact/movies/repositories/MovieRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
-    Optional<Movie> findByTitle(String title);
+    @Query("SELECT t FROM Movie t WHERE t.title = :title")
+    List<Movie> findByTitle(String title);
     @Query("SELECT m FROM Movie m WHERE m.imdb_id = :imdbId")
     Optional<Movie> findMovieByImdbId(@Param("imdbId") Long imdbId);
 }
