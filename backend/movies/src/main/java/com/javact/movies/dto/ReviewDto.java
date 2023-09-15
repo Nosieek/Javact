2f17b58d53b15b930ec2a5dde426d9ab37407ec2
Nosieek <mateusz.nosko13@gmail.com>
2023-09-15 16:48:52 +0200

    Added field with dateTime

--- backend/movies/src/main/java/com/javact/movies/dto/ReviewDto.java
+++ backend/movies/src/main/java/com/javact/movies/dto/ReviewDto.java
@@ -2,17 +2,25 @@ package com.javact.movies.dto;
 
 import lombok.Data;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Data
 public class ReviewDto {
     private Long id;
+    private String email;
     private String username;
     private int rating;
     private String comment;
+    private Date fullDate;
 
-    public ReviewDto(Long id, String username, int rating, String comment) {
+    public ReviewDto(Long id, String email,String username, int rating, String comment) {
         this.id = id;
+        this.email = email;
         this.username = username;
         this.rating = rating;
         this.comment = comment;
+        Calendar calendar = Calendar.getInstance();
+        this.fullDate = calendar.getTime();
     }
 }
